66653bd759e120ddba1593acc19edbc009140c6a
anish-kumar121 <anishkumar50752@gmail.com>
2024-08-29 22:29:45 +0530

    contactpage

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,9 @@
-import React from "react";
 import Contactform from "@/components/contact/Contactform";
 const Contact = () => {
   return (
     <div>
-      <Contactform />
+      {/* <Contactform /> */}
+      contact
     </div>
   );
 };
